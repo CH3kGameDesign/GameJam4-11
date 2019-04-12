9881972d65de536bc32f6ba9d1a194671176147f
Andrew <ROFLCOOPgame@gmail.com>
2019-04-12 11:55:29 +1000

    Added Projectile Rotation

--- GameJam4-11/Assets/Scripts/Projectile.cs
+++ GameJam4-11/Assets/Scripts/Projectile.cs
@@ -11,6 +11,27 @@ public class Projectile : MonoBehaviour
 
     public float m_spawnTime = 0.2f;
     private float m_lifeTime = 0.0f;
+    private void Update()
+    {
+        if (m_isLethal)
+        {
+            Vector3 dir = GetComponent<Rigidbody>().velocity;
+            dir.Normalize();
+            float dot = Vector3.Dot(transform.forward, dir);
+            float rotationAmount = Mathf.Cos(dot) * 2.5f; //multiplied by an arbitrary amount to simulate the weighted tip
+
+            float temp = Vector3.Dot(transform.up, dir);
+            
+            if (temp > 0.0f) // if greater than 0, object should rotate anti-clockwise
+            {
+                transform.Rotate(Vector3.right, -rotationAmount); // if object rotates counter-clockwise, change rotateAmount to -rotationAmount
+            }
+            else if (temp < 0.0f) // if less than 0, object should rotate clockwise
+            {
+                transform.Rotate(Vector3.right, rotationAmount); // if object rotates clockwise, change -rotateAmount to rotationAmount
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -38,5 +59,6 @@ public class Projectile : MonoBehaviour
             GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
             m_isLethal = false; // set bullet to non-lethal
         }
+
     }
 }
