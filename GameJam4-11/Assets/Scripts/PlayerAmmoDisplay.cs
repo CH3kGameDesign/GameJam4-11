b8f1251ed3b075dd524579b18b817d61bbf9f0d4
Dillon Handyside <dj_handyside@hotmail.com>
2019-04-12 11:24:47 +1000

    Implemented ammo display and added it to player prefab

---
+++ GameJam4-11/Assets/Scripts/PlayerAmmoDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAmmoDisplay : MonoBehaviour
+{
+    private PlayerFireScript m_playerFireScript; 
+
+    public SpriteRenderer[] m_ammoSprites;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_playerFireScript = GetComponent<PlayerFireScript>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        int numberOfProjectiles = m_playerFireScript.m_currentProjectiles;
+
+        for (int i = 0; i < m_ammoSprites.Length; i++)
+        {
+            if (i < numberOfProjectiles)
+                m_ammoSprites[i].enabled = true;
+            else
+                m_ammoSprites[i].enabled = false;
+        }
+	}
+}
